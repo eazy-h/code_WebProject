38ee525fdc4de65e0aece4966c9d4dd081fa96cc
easy-h <jhlee7129@gmail.com>
2019-05-02 16:46:22 +0900

    add TestCode, BoardDTO, MapperClass, Mapper.xml, CRUD(select, update, delete, insert)

---
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,22 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardDTO;
+
+public interface BoardMapper {
+//	@Select("select * from tbl_board where bno > 0")
+//	public List<BoardDTO> getList();
+	
+	//insert만 할때
+	public void insert(BoardDTO board);
+	//insert 후 bno을 알아야 할 때
+	public void insertSelectKey(BoardDTO board);
+	//게시물 읽기
+	public BoardDTO read(Long bno);
+	//게시물 삭제
+	public int delete(Long bno);
+	//게시물 수정
+	public int update(BoardDTO board);
+}
---
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,77 @@
+package org.zerock.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardDTO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper boardMapper;
+	
+	//BoardMapper 쿼리 테스트 메소드 명으로 실행 
+//	@Test
+//	public void testGetList() {
+//		boardMapper.getList().forEach(board -> log.info(board));
+//	}
+	
+	//basic insert
+//	@Test
+//	public void testInsert() {
+//		BoardDTO board = new BoardDTO();
+//		board.setTitle("New title");
+//		board.setContent("New content");
+//		board.setWriter("new Writer");
+//		
+//		boardMapper.insert(board);
+//		
+//		log.info(board);
+//	}
+	
+	//selectKey insert
+//	@Test
+//	public void testInsertSelectKey() {
+//		BoardDTO board = new BoardDTO();
+//		board.setTitle("셀렉트키 New title");
+//		board.setContent("셀렉트키 New content");
+//		board.setWriter("셀렉트키 new Writer");
+//		
+//		boardMapper.insertSelectKey(board);
+//		
+//		log.info(board);
+//	}
+	
+	//readOne
+//	@Test
+//	public void testRead() {
+//		BoardDTO board = boardMapper.read(5L);
+//		log.info(board);
+//	}
+	
+	//delete
+//	@Test
+//	public void testDelete() {
+//		log.info("delete Count : "+boardMapper.delete(3L));
+//	}
+	
+	//update
+	@Test
+	public void testUpdate() {
+		BoardDTO board = new BoardDTO();
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("수정될 일 없는 작성자");
+		int count=boardMapper.update(board);
+		log.info("update Count : "+count);
+	}
+}
